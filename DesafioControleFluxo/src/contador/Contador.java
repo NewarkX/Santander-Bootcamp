a32bba24faf59ba04d745f6647736bbac4ebdb88
Bruno <38993322+NewarkX@users.noreply.github.com>
2024-06-10 11:03:13 -0300

    DesafioControleFluxo
    E um contador que recebe dois parametros, valida se o primeiro e maior que o segundo, se for lanca uma excecao personalizada senao ele faz um  for imprimindo a diferenca entre o segundo o primeiro parametro.

---
+++ DesafioControleFluxo/src/contador/Contador.java
@@ -0,0 +1,33 @@
+package contador;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) throws ParametrosInvalidosException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parametro: ");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo parametro: ");
+        int parametroDois = scanner.nextInt();
+
+        contar(parametroUm, parametroDois);
+
+        scanner.close();
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println("Interação : " + i);
+        }
+    }
+}
+
